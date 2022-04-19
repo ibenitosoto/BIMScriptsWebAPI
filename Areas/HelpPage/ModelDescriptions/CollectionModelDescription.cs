2f1350efb9226da662e5a3258fe1e0f625559483
isoto <isoto@IT-22014-WST>
2022-04-19 11:48:02 +0100

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BIMScriptsWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
